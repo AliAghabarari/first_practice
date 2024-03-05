7ca8ce3d5aca9c7e811c80378bb3121ad7800117
AliAghabarari <aliaghabarari213@gmail.com>
2024-03-05 11:35:12 +0330

    5.py

---
+++ 5.py
@@ -0,0 +1,12 @@
+import turtle
+
+size = int(input("write size of your pen: "))
+color = input("write color of your pen: ")
+shape = turtle.Turtle()
+shape.shape('turtle')
+shape.color(color)
+for i in range(4):
+    shape.forward(100)
+    shape.left(90)
+
+turtle.mainloop()
